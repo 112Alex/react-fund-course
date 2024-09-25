1d7bb520e0fe542095ee5b0d1577a80a44117ca1
sasha <sasasuhinin170@gmail.com>
2024-09-25 19:56:18 +0300

    1:08:12

--- src/components/UI/button/input/MyInput.jsx
+++ src/components/UI/button/input/MyInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classes from './MyInput.module.css'
-
+// input
 const MyInput = React.forwardRef((props, ref) => {
   return (
     <input ref={ref} className={classes.myInput} {...props}/>
